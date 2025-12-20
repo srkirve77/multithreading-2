8ffcc88d2d06d2519698f34498f99f2ce669692e
Shubham Kirve <srkirve77@gmail.com>
2025-12-21 00:22:11 +0530

    init multithreading project

---
+++ src/main/java/com/example/multithreading/App.java
@@ -0,0 +1,8 @@
+package com.example.multithreading;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Main thread execution finished." );
+        System.out.println(Thread.currentThread().getName());
+    }
+}
