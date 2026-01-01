bbb80258f2a28e7683989769f3d509956625c2da
Shubham Kirve <srkirve77@gmail.com>
2026-01-01 15:25:52 +0530

    optimized register callbacks

--- src/main/java/com/example/multithreading/l_register_callback/RegisterCallbackV2.java
+++ src/main/java/com/example/multithreading/l_register_callback/RegisterCallbackV2.java
@@ -5,12 +5,13 @@ import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class RegisterCallbackV2 {
     public static void main(String[] args) throws InterruptedException {
         RegisterCallbackImplV2 registerCallbackImplV2 = new RegisterCallbackImplV2();
-        int noOfThreads = 100000;
+        int noOfThreads = 10000;
         List<Thread> threads = new ArrayList<>();
         long currentTime = System.currentTimeMillis();
         Random random = new Random();
@@ -39,8 +40,13 @@ class RegisterCallbackImplV2 {
     PriorityQueue<CallackConfigV2> pq3;
     CallbackResourceV2 callbackResourceV2;
     ReentrantLock lock1 = new ReentrantLock();
+    Condition condition1 = lock1.newCondition();
+
     ReentrantLock lock2 = new ReentrantLock();
+    Condition condition2 = lock2.newCondition();
+
     ReentrantLock lock3 = new ReentrantLock();
+    Condition condition3 = lock3.newCondition();
 
     public RegisterCallbackImplV2() {
         pq1 = new PriorityQueue<>(4000);
@@ -50,8 +56,11 @@ class RegisterCallbackImplV2 {
         Thread demonThread1 = new Thread(() -> {
             try {
                 while (true) {
-                    long currentTime = System.currentTimeMillis();
                     lock1.lock();
+                    long currentTime = System.currentTimeMillis();
+                    while (pq1.isEmpty()) {
+                        condition1.await();
+                    }
                     while (!pq1.isEmpty() && pq1.peek().callbackTime <= currentTime) {
                         CallackConfigV2 callackConfig = pq1.remove();
                         callbackResourceV2.callback(callackConfig);
@@ -68,8 +77,11 @@ class RegisterCallbackImplV2 {
         Thread demonThread2 = new Thread(() -> {
             try {
                 while (true) {
-                    long currentTime = System.currentTimeMillis();
                     lock2.lock();
+                    long currentTime = System.currentTimeMillis();
+                    while (pq2.isEmpty()) {
+                        condition2.await();
+                    }
                     while (!pq2.isEmpty() && pq2.peek().callbackTime <= currentTime) {
                         CallackConfigV2 callackConfig = pq2.remove();
                         callbackResourceV2.callback(callackConfig);
@@ -86,8 +98,11 @@ class RegisterCallbackImplV2 {
         Thread demonThread3 = new Thread(() -> {
             try {
                 while (true) {
-                    long currentTime = System.currentTimeMillis();
                     lock3.lock();
+                    long currentTime = System.currentTimeMillis();
+                    while (pq3.isEmpty()) {
+                        condition3.await();
+                    }
                     while (!pq3.isEmpty() && pq3.peek().callbackTime <= currentTime) {
                         CallackConfigV2 callackConfig = pq3.remove();
                         callbackResourceV2.callback(callackConfig);
@@ -106,18 +121,29 @@ class RegisterCallbackImplV2 {
         demonThread3.start();
     }
 
+    public static long sleepFor(long currentTime, long callbackTime) {
+        long additionalLiveliness = 10;
+        if (currentTime > callbackTime + additionalLiveliness) {
+            return currentTime - callbackTime - additionalLiveliness;
+        }
+        return 1;
+    }
+
     public void registerCallback(CallackConfigV2 callackConfig) {
         if (callackConfig.threadNo % 3 == 0) {
             lock1.lock();
             pq1.add(callackConfig);
+            condition1.signal();
             lock1.unlock();
         } else if (callackConfig.threadNo % 3 == 1) {
             lock2.lock();
             pq2.add(callackConfig);
+            condition2.signal();
             lock2.unlock();
         } else if (callackConfig.threadNo % 3 == 2) {
             lock3.lock();
             pq3.add(callackConfig);
+            condition3.signal();
             lock3.unlock();
         }
     }
